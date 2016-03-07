89c6a71e63dfb8680e821fe70e192e4c109015a2
Nathan Otto <ottoetc@gmail.com>
2016-03-07 22:18:37 +0000

    add classes

--- Objects/Card.cs
+++ Objects/Card.cs
@@ -8,14 +8,87 @@ namespace MemoryGame
       private int _id;
       private string _cardBack;
       private string _cardFront;
+      private int _pairNum;
       
       
+      public Card(int cardBack, string cardFront, int Id = 0)
+      {
+        _cardBack = cardBack;
+        _cardFront = cardFront;
+        _id = Id;
+        _pairNum = 
+      }
+      
+      public override bool Equals(System.Object otherCard)
+      {
+        if(!(otherCard is Card))
+        {
+          return false;
+        }
+        else
+        {
+          Card newCard = (Card) otherCard;
+          bool idEquality = this.GetId() == newCard.GetId();
+          bool cardBackEquality = this.GetCardBack() == newCard.GetCardBack();
+          bool cardFrontEquality = this.GetCardFront() == newCard.GetCardFront();
+          return (idEquality && cardBackEquality && cardFrontEquality);
+        }
+      }
+      
+      public int GetId()
+      {
+        return _id;
+      }
+      public string GetCardBack()
+      {
+        return _cardBack;
+      }
+      public void SetCardBack(string newCardBack)
+      {
+        _cardBack = newCardBack;
+      }
+      public string GetCardFront()
+      {
+        return _cardFront;
+      }
+      public void SetCardFront(string newCardFront)
+      {
+        _cardFront = newCardFront;
+      }
+      
+      public void Save()
+      {
+        SqlConnection conn = DB.Connection();
+        SqlDataReader rdr;
+        conn.Open();
+        
+        SqlCommand cmd = new SqlCommand("INSERT INTO cards (cardback, cardfront) OUTPUT INSERTED.id VALUES(@CardBack, @CardFront);"; conn);
+        
+        SqlParameter cardBackParameter = new SqlParameter();
+        cardBackParameter.ParameterName = "@CardBack";
+        cardBackParameter.Value = this.GetCardBack();
+        
+        SqlParameter cardFrontParameter = new SqlParameter();
+        cardFrontParameter.ParameterName = "@CardFront";
+        cardFrontParameter.Value = this.GetCardFront();
+        
+        cmd.Parameters.Add(cardFrontParameter);
+        cmd.Parameters.Add(cardBackParameter);
+        
+        while(rdr.Read())
+        {
+          this._id = rdr.GetInt32(0);
+        }
+        if(rdr != null)
+        {
+          rdr.Close();
+        }
+        if(conn != null)
+        {
+          conn.Close();
+        }
+      }
       
       
-      
-      
-      
-      
-      public Card(string CardBack)
     }
   }
--- Objects/Game.cs
+++ Objects/Game.cs
@@ -7,6 +7,23 @@ namespace MemoryGame
     {
       private int _id;
       
+      
+      
+      
+      public Game(int Difficulty, int Theme)
+      
+      
+      for(i = 0; difficulty.Length; i++)
+      {
+        int randOrder = random generated # 1-100
+        int randOrder2 = random generated # 1-100
+      card newcard(i) = new Card(int Theme, pairNum i, randOrder)
+      Card new2card(i) = new Card(int Theme, pairNum i, randOrder2)
+      
+      }
+      
+      <img src = "/img/@Model.GetTheme()@model.GetPairNum().jpg"
+                  /img/harrypotter12.jpg
     }
   }
 
