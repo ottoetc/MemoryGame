dc02d6c0f38d63fcae4f07a3575a92520b0ecb84
Nathan Otto <ottoetc@gmail.com>
2016-03-08 00:15:46 +0000

    Add deleteall and getall to card.cs

--- Objects/Card.cs
+++ Objects/Card.cs
@@ -6,17 +6,17 @@ namespace MemoryGame
     public class Card
     {
       private int _id;
-      private string _cardBack;
-      private string _cardFront;
+      private string _theme
       private int _pairNum;
+      private int _randNum;
       
       
-      public Card(int cardBack, string cardFront, int Id = 0)
+      public Card(string Theme, int PairNum, int RandNum, int Id = 0)
       {
-        _cardBack = cardBack;
-        _cardFront = cardFront;
+        _theme = Theme;
         _id = Id;
-        _pairNum = 
+        _pairNum = PairNum;
+        _randNum = RandNum
       }
       
       public override bool Equals(System.Object otherCard)
@@ -29,9 +29,10 @@ namespace MemoryGame
         {
           Card newCard = (Card) otherCard;
           bool idEquality = this.GetId() == newCard.GetId();
-          bool cardBackEquality = this.GetCardBack() == newCard.GetCardBack();
-          bool cardFrontEquality = this.GetCardFront() == newCard.GetCardFront();
-          return (idEquality && cardBackEquality && cardFrontEquality);
+          bool themeEquality = this.GetTheme() == newCard.GetTheme();
+          bool pairNumEquality = this.GetPairNum() == newCard.GetPairNum();
+          bool randNumEquality = this.GetRandNum() == newCard.GetRandNum();
+          return (idEquality && themeEquality && pairNumEquality && randNumEquality);
         }
       }
       
@@ -39,21 +40,29 @@ namespace MemoryGame
       {
         return _id;
       }
-      public string GetCardBack()
+      public int GetPairNum()
       {
-        return _cardBack;
+        return _pairNum;
       }
-      public void SetCardBack(string newCardBack)
-      {
-        _cardBack = newCardBack;
+      public void SetPairNum(int newPairNum)
+      {   
+        _pairNum = newPairNum;
       }
-      public string GetCardFront()
+      public int GetRandNum()
       {
-        return _cardFront;
+        return _randNum;
       }
-      public void SetCardFront(string newCardFront)
+      public void SetRandNum(int newRandNum)
       {
-        _cardFront = newCardFront;
+        _randNum = newRandNum;
+      }
+      public string GetTheme()
+      {
+        return _theme;
+      }
+      public void SetTheme(string newTheme)
+      {
+        _theme = newTheme;
       }
       
       public void Save()
@@ -62,18 +71,23 @@ namespace MemoryGame
         SqlDataReader rdr;
         conn.Open();
         
-        SqlCommand cmd = new SqlCommand("INSERT INTO cards (cardback, cardfront) OUTPUT INSERTED.id VALUES(@CardBack, @CardFront);"; conn);
+        SqlCommand cmd = new SqlCommand("INSERT INTO cards (theme, pairnum, randnum) OUTPUT INSERTED.id VALUES(@Theme, @PairNum, @RandNum);", conn);
         
-        SqlParameter cardBackParameter = new SqlParameter();
-        cardBackParameter.ParameterName = "@CardBack";
-        cardBackParameter.Value = this.GetCardBack();
+        SqlParameter themeParameter = new SqlParameter();
+        themeParameter.ParameterName = "@Theme";
+        themeParameter.Value = this.GetTheme();
         
-        SqlParameter cardFrontParameter = new SqlParameter();
-        cardFrontParameter.ParameterName = "@CardFront";
-        cardFrontParameter.Value = this.GetCardFront();
+        SqlParameter pairNumParameter = new SqlParameter();
+        pairNumParameter.ParameterName = "@PairNum";
+        pairNumParameter.Value = this.GetPairNum();
         
-        cmd.Parameters.Add(cardFrontParameter);
-        cmd.Parameters.Add(cardBackParameter);
+        SqlParameter randNumParameter = new SqlParameter();
+        randNumParameter.ParameterName = "@RandNum";
+        randNumParameter.Value = this.GetRandNum();
+        
+        cmd.Parameters.Add(themeParameter);
+        cmd.Parameters.Add(pairNumParameter);
+        cmd.Parameters.Add(randNumParameter);
         
         while(rdr.Read())
         {
@@ -88,7 +102,46 @@ namespace MemoryGame
           conn.Close();
         }
       }
-      
+      public static List<Card> GetAll()
+      {
+        List<Card> allCards = new List<Card>{};
+        
+        SqlConnection conn = DB.Connection();
+        SqlDataReader rdr = null;
+        conn.Open();
+        
+        SqlCommand cmd = new SqlCommand("SELECT * FROM cards ORDER BY randnum ASC;", conn);
+        rdr = cmd.ExecuteReader();
+        
+        while(rdr.Read())
+        {
+          int cardId = rdr.GetInt32(0);
+          string cardTheme = rdr.GetString(1);
+          int cardPairNum = GetInt32(2);
+          int cardRandNum = GetInt32(3);
+          
+          Card newCard = new Card(cardTheme, cardPairNum, cardRandNum, cardId);
+          allCards.Add(newCard);
+        }
+        if(rdr != null)
+        {
+          rdr.Close();
+        }
+        if(rdr != null)
+        {
+          conn.Close();
+        }
+        return allCards;
+      }
+      public static void DeleteAll()
+      {
+        SqlConnection conn = DB.Connection();
+        SqlDataReader rdr = null;
+        conn.Open();
+        
+        SqlCommand cmd = new SqlCommand("DELETE FROM cards; DBCC CHECKIDENT ('cards', RESEED, 0);", conn);
+        cmd.ExecuteNonQuery();
+      }
       
     }
   }
--- Objects/Game.cs
+++ Objects/Game.cs
@@ -6,24 +6,30 @@ namespace MemoryGame
     public class Game
     {
       private int _id;
+      private string _Theme
       
       
       
-      
-      public Game(int Difficulty, int Theme)
+      public Game(int Difficulty, string Theme)
       
       
       for(i = 0; difficulty.Length; i++)
       {
-        int randOrder = random generated # 1-100
-        int randOrder2 = random generated # 1-100
+       int randOrder = random generated # 1-1000000
+        int randOrder2 = random generated # 1-1000000
       card newcard(i) = new Card(int Theme, pairNum i, randOrder)
       Card new2card(i) = new Card(int Theme, pairNum i, randOrder2)
-      
       }
       
-      <img src = "/img/@Model.GetTheme()@model.GetPairNum().jpg"
+     //cardfront
+      <img src = "/img/@model.GetTheme()@model.GetPairNum().jpg"
                   /img/harrypotter12.jpg
+                  
+    //cardback
+      <img src= "/img/CardBack@Model.GetTheme().jpg"
+                /img/CardBack2.jpg
+                /img/CardBackHarryPotter.jpg
+                
     }
   }
 
