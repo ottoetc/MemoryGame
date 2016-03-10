5bb5fc1dc1d3dde8173a3d68f0fae90efc17c244
Nathan Otto <ottoetc@gmail.com>
2016-03-10 00:08:52 +0000

    add homemodule layout

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -13,22 +13,50 @@ namespace MemoryGame
       {
         return View["index.cshtml"];
       };
-      Post["/"] = _ =>
+      Post["/game"] = _ = >
       {
-        Game newGame = new Game(2, "harrypotter", 1);
-        
-        Card testCard = new Card("harrypotter", 1, 15, 1);
-        Card testCard2 = new Card("harrypotter", 1, 15, 2);
-        
-        testCard.Save();
-        testCard2.Save();
-        
-        Card newCard = Card.Find(Request.Form["card1"];
-        Card newCard2 = Card.Find(Request.Form["card2"];
-        
-        bool result = Game.Check(newCard, newCard2);
-        return View["index.cshtml", result];
+        Game newGame = new Game(12, "harrypotter");
+        return View[""]
       };
+      
+      Get[] = _ =>
+      {
+        
+      };
+      Post["/firstpick"] = _ =>
+      {
+   
+        List<Card> gameCards = newGame.CreateGame();
+        List<Card> randomCards = Card.GetAll();
+        Card clickedCard1 = Card.Find(Request.Form["clicked-card"]);
+        // bool result = Game.Check(newCard, newCard2);
+        clickedCard1.SetFirstCard();
+        
+        List<Card> updatedBoard = Card.GetAll();
+        return View["game.cshtml", updatedBoard];
+      };
+      
+      Get[] = _ =>
+      {
+        
+      };
+      Post["/secondpick"] = _ =>
+      {
+        Card clickedCard2 = Card.Find(Request.Form["clicked-card2"]);
+        Card clickedCard1 = Card.GetFirstCard();
+        bool result = Game.Check(clickedCard1, clickedCard2);
+        if(result == true)
+        {
+        bool winner = Game.CheckWin();
+        }
+        if(winner == true)
+        {
+          return View["winner.cshtml"];
+        }
+        List<Card> updatedBoard = Card.GetAll();
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        model.Add("")
+      }
     }
   }
 }
